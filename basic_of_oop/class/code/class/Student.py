58c5c34c41d19e3d0523e8e7b8f9bbce685c1cc4
Md. Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-03-10 19:46:18 +0600

    instance method created

--- basic_of_oop/class/code/class/Student.py
+++ basic_of_oop/class/code/class/Student.py
@@ -1,6 +1,13 @@
 class Student:
     # default constructor
-    def __init__(self):
-        print("a student object created")
-s1 = Student()
-print(s1)
+    def __init__(self,student_name,student_id):
+        self.student_name = student_name # instance variable
+        self.student_id = student_id     # instance variable
+        #print(self.student_name, self.student_id)
+
+    def display(self):
+        print("Name: ", self.student_name, "Id: ",self.student_id)
+
+s1 = Student("Bob",12)
+s1.display()
+
