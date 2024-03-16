80f0a761afc8c9bf03be459e7255fa2fc1014dc3
Md. Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-03-16 12:49:34 +0600

    return object in operator overloading

--- basic_of_oop/operator_overloading/code/operator_overloading.py
+++ basic_of_oop/operator_overloading/code/operator_overloading.py
@@ -8,10 +8,12 @@ class Home:
     def __add__(self,other):
         total_windows = self.window + other.window 
         total_doors = self.door + other.door   
-        return f"total door {total_doors} and total window {total_windows}"
+        return Home(total_doors,total_windows)
+        #return f"total door {total_doors} and total window {total_windows}"
 
 h1 = Home(12,22)
 h2 = Home(33,444)
 print(h1.view())
 print(h2.view())
-print(h1+h2)
+h3 = h1+h2
+print(h3.view())
