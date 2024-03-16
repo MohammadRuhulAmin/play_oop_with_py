e589583ac96a7245ca8def5ecf4579095a1c5a25
Md. Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-03-16 12:43:00 +0600

    operator overloading real life example added

--- basic_of_oop/operator_overloading/code/operator_overloading.py
+++ basic_of_oop/operator_overloading/code/operator_overloading.py
@@ -1,12 +1,17 @@
-class data: 
-    def __init__(self, x):
-        self.x = x 
-    def __add__(self,other):
-        return self.x + other.x 
-    def __sub__(self, other):
-        return self.x - other.x 
+class Home:
+    def __init__(self,window,door):
+        self.window = window
+        self.door = door 
     
-num1 = data(12)
-num2 = data(34)
-print(num1+num2)
-print(num1-num2)
+    def view(self):
+        return f"total door = {self.door} and total window = {self.window}"
+    def __add__(self,other):
+        total_windows = self.window + other.window 
+        total_doors = self.door + other.door   
+        return f"total door {total_doors} and total window {total_windows}"
+
+h1 = Home(12,22)
+h2 = Home(33,444)
+print(h1.view())
+print(h2.view())
+print(h1+h2)
--- basic_of_oop/operator_overloading/concept/operator_overloading.md
+++ basic_of_oop/operator_overloading/concept/operator_overloading.md
@@ -42,3 +42,26 @@ num2 = data(34)
 print(num1+num2)
 print(num1-num2)
 ```
+
+<b>Real Life Example </b>
+
+```python
+class Home:
+    def __init__(self,window,door):
+        self.window = window
+        self.door = door 
+    
+    def view(self):
+        return f"total door = {self.door} and total window = {self.window}"
+    def __add__(self,other):
+        total_windows = self.window + other.window 
+        total_doors = self.door + other.door   
+        return f"total door {total_doors} and total window {total_windows}"
+
+h1 = Home(12,22)
+h2 = Home(33,444)
+print(h1.view())
+print(h2.view())
+print(h1+h2)
+
+```
